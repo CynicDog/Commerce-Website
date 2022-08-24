405df6e310eedbaac0dd53b97b381a0edebc5ecd
eunsang_lee <thecynicdog0328@gmail.com>
2022-08-24 15:39:59 +0900

    init

---
+++ CommerceWebsite_Backend/src/main/java/com/example/commercewebsite_backend/CommerceWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.commercewebsite_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceWebsiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommerceWebsiteBackendApplication.class, args);
+    }
+
+}
