c0d12d1e08e18ed7d1ccbe5358c62bd7bb904b24
eunsang_lee <thecynicdog0328@gmail.com>
2022-08-26 13:30:43 +0900

    rename

--- CommerceWebsite_Backend/src/main/java/com/example/commercewebsite_backend/repository/ProductRepository.java
+++ CommerceWebsite_Backend/src/main/java/com/example/commercewebsite_backend/repository/ProductRepository.java
@@ -3,8 +3,10 @@ package com.example.commercewebsite_backend.repository;
 import com.example.commercewebsite_backend.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "product", path = "product")
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
