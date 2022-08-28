d150d34338d8f606e3a70efc1dff9a63d833fcd7
eunsang_lee <thecynicdog0328@gmail.com>
2022-08-28 19:51:05 +0900

    feat

--- CommerceWebsite_Backend/src/main/java/com/example/commercewebsite_backend/configuration/RestConfig.java
+++ CommerceWebsite_Backend/src/main/java/com/example/commercewebsite_backend/configuration/RestConfig.java
@@ -2,15 +2,28 @@ package com.example.commercewebsite_backend.configuration;
 
 import com.example.commercewebsite_backend.domain.Product;
 import com.example.commercewebsite_backend.domain.ProductCategory;
-import com.example.commercewebsite_backend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import javax.persistence.metamodel.EntityType;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Configuration
 public class RestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public RestConfig(EntityManager _entityManager) {
+        entityManager = _entityManager;
+    }
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -32,5 +45,20 @@ public class RestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        List<Class> entityClasses = new ArrayList<>();
+        for (EntityType entityType : entities) { entityClasses.add(entityType.getJavaType()); }
+
+        Class[] domainType = entityClasses.toArray(new Class[0]);
+
+        config.exposeIdsFor(domainType);
     }
 }
+
--- CommerceWebsite_Backend/src/main/java/com/example/commercewebsite_backend/repository/ProductRepository.java
+++ CommerceWebsite_Backend/src/main/java/com/example/commercewebsite_backend/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.example.commercewebsite_backend.repository;
 
 import com.example.commercewebsite_backend.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,4 +12,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
